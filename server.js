872f9732353c3bfebee399c3ffc5290251cacc9f
Samudra Harapan Bekti <samudra.bekti@gmail.com>
2015-07-16 16:50:22 +0700

    Add simple analytics

--- server.js
+++ server.js
@@ -5,15 +5,44 @@ var io = require('socket.io')(http);
 var path = require('path');
 var request = require('request');
 
+var onlineUsers = 0;
+var userCount = {};
+
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/stats', function(req, res) {
+  var payload = {
+    onlineUsers: onlineUsers,
+    userCount: userCount
+  };
+
+  res.json(payload);
+});
+
 io.on('connection', function(socket) {
+  ++onlineUsers;
+
+  var today = new Date().toJSON().slice(0,10);
+  var todayData = userCount[today];
+
+  if (todayData != undefined) {
+    userCount[today].count++;
+  } else {
+    userCount[today] = {
+      count: 1
+    };
+  }
+
   socket.on('location', function(data) {
     fetchGojekDrivers(data, socket);
+  });
+
+  socket.on('disconnect', function() {
+    --onlineUsers;
   });
 });
 
